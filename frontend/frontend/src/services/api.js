a48446d2f58c517270ff34b424b333d9cf17d030
Afhaan <afhaan247@gmail.com>
2026-02-08 18:51:59 +0530

    use railway backend url

--- frontend/frontend/src/services/api.js
+++ frontend/frontend/src/services/api.js
@@ -1,4 +1,5 @@
-const API_URL = "http://127.0.0.1:8000/analyze";
+const API_URL = "https://fearless-empathy-production.up.railway.app/analyze";
+
 
 export async function analyzeResume(file, jobDescription) {
   const formData = new FormData();
